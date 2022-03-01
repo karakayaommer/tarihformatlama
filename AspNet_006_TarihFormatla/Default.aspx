b2715ed0d1402e3d7f78cb72016c35eb208736a9
Ömer <62448865+karakayaommer@users.noreply.github.com>
2022-03-01 23:05:57 +0300

    Add files via upload

---
+++ AspNet_006_TarihFormatla/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AspNet_006_TarihFormatla.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="btTarihGoster" runat="server" OnClick="btTarihGoster_Click" Text="Button" />
+        <br />
+        <asp:Label ID="lbTarihVarsayilan" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="lbTarihFormatli" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ AspNet_006_TarihFormatla/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNet_006_TarihFormatla
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btTarihGoster_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Today;
+            lbTarihVarsayilan.Text = bugun.ToString();
+            string formatliTarih = String.Format("{0:dd.MM.yyyy}", bugun);
+            lbTarihFormatli.Text = formatliTarih;
+        }
+    }
+}
